168b1b870944ad6d13e117d35db3e0971d540d8c
Vaibhavgp7 <vaibhavkg7@gmail.com>
2022-12-15 23:47:11 +0530

    added backend app url

--- phonefrontend/src/services/persons.js
+++ phonefrontend/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
-
+const baseUrl = 'https://phonebook-backend-sljo.onrender.com/api/persons'
+//'http://localhost:3001/api/persons'
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
